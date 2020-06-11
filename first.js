0796b35976e6841cd0c51778ebcf3f6a99c10946
aakashDeveloper <ahanda205@gmail.com>
2020-06-11 08:44:35 +0530

    a

---
+++ first.js
@@ -0,0 +1,139 @@
+Es5
+Es6
+Es7
+Ecma Script 
+
+Short Hand 
+New Feature
+New Functionality
+
+>>>>>>>> Es6 
+>Some browser soome dose not support Es6
+> Transpile (.babel)
+
+//////////////////////////////////////////////////
+DataType 
+////////////////
+
+String  "5344" "hi" 'rrtrrgrt' '44rgre'
+Number  7 454376846786 7467.4875678
+Boolean true/false
+null
+undefined
+
+//////////////////////////
+           Es5
+/////////////////////////
+
+var a = 10
+var b = "Hi"
+var c = true
+
+var a = 10
+var b = "Hi"
+var c = true
+
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d = "445645"
+typeof(d)
+"string"
+
+var a = 10
+undefined
+var b = 20
+undefined
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+modules
+4%2
+0
+4%3
+1
+4%4
+0
+
+2%4
+3%4
+
+modules of smaller to larger is always smaller
+
+var a = "hi"
+undefined
+var b = " JavaScript"
+undefined
+a+b
+"hi JavaScript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+var a = "hi"
+undefined
+var b = 10
+undefined
+a+b
+"hi10"
+b+a
+"10hi"
+a-b
+NaN
+b-a
+NaN(not a number)
+
++ with number add 
++ with string conact
+
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+"20"+10+"20"
+"201020"
+"20"+10+10
+"201010"
+10+10+"20"
+"2020"
+
+"20"+10+"20"-1
+201019
+"20"+10+10-1
+201009
+10+10+"20"-1
+2019
+
+
+"10"-1
+NaN
+
+"10"-'a'
+NaN
+"10"-"1"
+9
+"10a"-"1"
+NaN
+"10"+"2"
+"102"
+"10"-"2"
+8
+"10a"-"2"
+NaN
+
+
