cb280803f3faf0003faec13ce10854b89bfaacc8
aakashDeveloper <ahanda205@gmail.com>
2020-06-12 08:32:40 +0530

    A

---
+++ second.js
@@ -0,0 +1,109 @@
+Boolean
+true is equal to 1
+false is equal to 0
+
+true+true
+
+true+true
+2
+true+false
+1
+true-true
+0
+true+2
+3
+true*2
+2
+true/2
+0.5
+true%2
+1
+true+"hii"
+"truehii"
+true-"hii"
+NaN
+"1"-"1"
+0
+"true"-"true"
+NaN
+
+parseInt()> convert string number into pure number
+//////////////////
+var a = "1"
+var b = "2"
+undefined
+a+b
+"12"
+parseInt(a)
+1
+parseInt(b)
+2
+parseInt(a)+parseInt(b)
+3
+parseInt(a+b)
+12
+
+
+var a = "1.322"
+var b = "2.343"
+
+var c = "ddf"
+parseInt(c)
+NaN
+
+
+var a = "1.322"
+var b = "2.343"
+undefined
+parseInt(a)+parseInt(b)
+3
+parseFloat(a)
+1.322
+parseFloat(b)
+2.343
+parseFloat(a)+parseFloat(b)
+3.665
+
+
+var a = "1"
+undefined
+parseFloat(a)
+1
+var a = 78
+undefined
+parseFloat(a)
+78
+
+var a = "3"
+undefined
+typeof(a)
+"string"
+var a = 3
+undefined
+typeof(a)
+"number"
+
+
+var a  = "dfe"
+undefined
+isNaN(a) > is not a number
+true
+var b = 10
+undefined
+isNaN(b)
+false
+
+
+> for string it will return true
+> for number it will return false
+
+
+var c = true
+isNaN(c)
+false
+var d = false
+undefined
+isNaN(d)
+false
+
+
---
+++ third.js
@@ -0,0 +1,34 @@
+console.log() > debuging and check output
+alert() > for give message
+prompt() > to take input for user
+confirm() > to take confirmation
+
+
+console.log("hiiiii")
+VM1289:1 hiiiii
+undefined
+alert("Form Submitted")
+undefined
+
+prompt("What is your age?")
+"33"
+
+var a = prompt("Enter first Value")
+var b = prompt("Enter Second value")
+var out = parseFloat(a)+parseFloat(b)
+alert(out)
+
+confirm("Do you want to leave?")
+
+confirm("Do you want to leave?")
+true
+confirm("Do you want to leave?")
+false
+var a = confirm("Do you want to leave?")
+undefined
+a
+true
+var a = confirm("Do you want to leave?")
+undefined
+a
+false
