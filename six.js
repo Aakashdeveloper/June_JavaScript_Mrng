35f2e2e10b4a28e61cb66a024a6849975c763c8c
aakashDeveloper <ahanda205@gmail.com>
2020-06-17 09:06:07 +0530

    a

---
+++ six.js
@@ -0,0 +1,64 @@
+= assignment
+== compare the data
+=== compare data and datatype
+
+
+var a = 10
+var b = "10"
+
+var a = 10
+var b = "10"
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+a == b
+true
+a === b
+false
+
+var a = 10
+var b = "10"
+undefined
+var c = 20
+undefined
+a == c
+false
+a != c
+true
+a !== c
+true
+
+
+(a==b)  ===> (a!=b)
+(a===b) ===> (a!==b)
+
+var a =true
+
+var a =true
+undefined
+!a
+false
+var a =false
+undefined
+!a
+true
+var b = !a
+undefined
+b
+true
+!b
+false
+
+var a = 1
+!a
+
+var a = 1
+undefined
+!a
+false
+var b =0
+undefined
+!b
+true
