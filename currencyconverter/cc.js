778b121281a9b053806c81b20e01cbfbc88eb016
ahand14 <aakash.handa@aexp.com>
2020-07-04 08:39:28 +0530

    a

--- currencyconverter/cc.js
+++ currencyconverter/cc.js
@@ -1,6 +1,19 @@
 var url = "https://api.exchangeratesapi.io/latest?base="
 
-function convert(){
+async function convert(){
+    var base = document.getElementById('base').value;
+    var out = document.getElementById('output').value;
+    var amount = document.getElementById('amount').value;
+    var final;
+    let response = await fetch(`${url}${base}`,{method:'GET'})
+    let data = await response.json()
+    final = data.rates;
+    final = (final[out]*parseFloat(amount))
+    var result = `Converted value of ${amount} ${base} is ${final} ${out}`
+    document.getElementById('converted').innerHTML=result;  
+}
+
+/*function convert(){
     var base = document.getElementById('base').value;
     var out = document.getElementById('output').value;
     var amount = document.getElementById('amount').value;
@@ -15,4 +28,4 @@ function convert(){
     })
 
     
-}
+}*/
---
+++ jquery/first.js
@@ -0,0 +1,10 @@
+$('h1')
+m.fn.init [h1, prevObject: m.fn.init(1), context: document, selector: "h1"]
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
+$('.scroll')
+m.fn.init(5) [a.scroll, a.scroll, a.scroll, a.scroll, a.scroll, prevObject: m.fn.init(1), context: document, selector: ".scroll"]
+$('.scroll')[0]
+<a href=​"#skills" class=​"scroll">​Skills​</a>​
+$('.scroll')[0].innerText="Tech"
+"Tech"
